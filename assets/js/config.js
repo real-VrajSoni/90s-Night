bf0c323ce3f0f2c036b7ba177a5746cffe038b79
VrajSoni <vrajsoni624@gmail.com>
2026-08-13 14:29:47 +0530

    creator links

--- assets/js/config.js
+++ assets/js/config.js
@@ -21,9 +21,9 @@ const SITE = {
 	developer: {
 		name: "Vraj Soni", // shown big on the card
 		role: "Front-end & product", // one short line under the name
-		instagram: "https://www.instagram.com/PASTE_YOUR_HANDLE", // <-- your Instagram
-		x: "https://x.com/PASTE_YOUR_HANDLE", // <-- your X / Twitter
-		linkedin: "https://www.linkedin.com/in/PASTE_YOUR_HANDLE", // <-- your LinkedIn
+		instagram: "https://www.instagram.com/vrajsoni77/", // <-- your Instagram
+		x: "https://x.com/the_vraj_soni", // <-- your X / Twitter
+		linkedin: "https://www.linkedin.com/in/vraj-soni-65095a282/", // <-- your LinkedIn
 	},
 
 	/* --------------------------------------------------------------------------
