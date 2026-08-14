4dfcb9597c11df591a69b9fa19c95fb3930e5409
VrajSoni <vrajsoni624@gmail.com>
2026-08-14 10:45:58 +0530

    developer profile

--- assets/js/config.js
+++ assets/js/config.js
@@ -20,7 +20,7 @@ const SITE = {
      ------------------------------------------------------------------------ */
 	developer: {
 		name: "Vraj Soni", // shown big on the card
-		role: "Front-end & product", // one short line under the name
+		role: "Software & product Engineer", // one short line under the name
 		instagram: "https://www.instagram.com/vrajsoni77/", // <-- your Instagram
 		x: "https://x.com/the_vraj_soni", // <-- your X / Twitter
 		linkedin: "https://www.linkedin.com/in/vraj-soni-65095a282/", // <-- your LinkedIn
